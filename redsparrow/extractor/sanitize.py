29670c3985e018ebfe733529b29c354ee0d6fe21
Odiate <makara.jan@student.pk.edu.pl>
2014-10-28 17:12:57 +0100

    sanitize
    preproces tekstu

---
+++ redsparrow/extractor/sanitize.py
@@ -0,0 +1,20 @@
+import string
+# sanitize
+
+sanitizedChars = "\n\r\t\ \f!()-[]{};:'\"\,<>./?@#$%^&*_~"
+
+def sanitize(removedChars):
+
+    def remove(s):
+        return ''.join(ch for ch in s if not ch in removedChars)
+    return remove
+
+sanitizedString = sanitize(sanitizedChars)
+
+#-> usage
+# test = "read this asdasdas,.,as;short text somethingr                 ead\"\"\"\::thisas\n././././,..,.,.'';';';';';'.,.,.,;l;';'1!!dasdasasshorttext'"
+# print(sanitizedString(test))  
+
+#winnowing
+
+# downcasing
