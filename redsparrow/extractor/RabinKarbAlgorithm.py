7e79c0fc0b9d6f3f0d270dd5b4192f0e67cb92fa
vanluke22 <vanluke@op.pl>
2014-10-28 18:40:20 +0100

    Add test and KarpRabin alg

---
+++ redsparrow/extractor/RabinKarbAlgorithm.py
@@ -0,0 +1,32 @@
+class RabinKarbAlgorithm:
+    """Rabin Karp implementation"""
+    def Calculate(self, text, pattern, d, q):
+        if text == None or pattern == None:
+            return -1
+        if text == "" or pattern == "":
+            return -1
+        if len(pattern) > len(text):
+            return -1
+        n = len(text)
+        m = len(pattern)
+        h = pow(d,m-1)%q
+        p = 0
+        t = 0
+        result = []
+        for i in range(m): 
+            p = (d*p+ord(pattern[i]))%q
+            t = (d*t+ord(text[i]))%q
+        for s in range(n-m+1):
+            if p == t: # check character by character
+                match = True
+                for i in range(m):
+                    if pattern[i] != text[s+i]:
+                        match = False
+                        break
+                if match:
+                    result = result + [s]
+            if s < n-m:
+                t = (t-h*ord(text[s]))%q
+                t = (t*d+ord(text[s+m]))%q 
+                t = (t+q)%q
+        return result
---
+++ tests/model/testClass.py
@@ -0,0 +1,6 @@
+class testClass(object):
+    """description of class"""
+    def __init__(self, string1, string2, result):
+        self.string1 = string1;
+        self.string2 = string2;
+        self.result = result;
