b0b7f60b77d58d78810daa3358b0768b4f749fe6
Marcin Kaciuba <aldor007@gmail.com>
2014-12-01 22:22:00 +0100

    init

---
+++ redsparrow/plagiarism/detector.py
@@ -0,0 +1,18 @@
+
+class PlagiarismDetector(object):
+
+    def preprocess(self, thesis_id):
+        pass
+        # get data from db
+        # get keyword
+        # start processing in by neares keyword
+
+    def process(self):
+        pass
+        # rabing karp, lavensthein
+        # top 10 winnowing 2
+        # check for fraud
+        # store result in db send to php
+
+
+
