b637de84d138febdd57982aff92115aeb421d3f2
Odiate <makara.jan@student.pk.edu.pl>
2014-12-06 15:34:50 +0100

    sanitize returning string
    fooling travis, fix ;p

---
+++ redsparrow/extractor/sanitize.py
@@ -0,0 +1,19 @@
+import string
+# sanitize
+
+sanitizedChars = "\n\r\t\ \f!()-[]{};:'\"\,<>./?@#$%^&*_~"
+
+def sanitize(removedChars):
+
+    def remove(s):
+        return ''.join(ch.lower() for ch in s if not ch in removedChars)
+    return remove
+
+#returning string
+sanitizedString = sanitize(sanitizedChars)
+
+#-> usage
+# test = "read this asdasdas,.,as;short text somethingr                 ead\"\"\"\::thisas\n././././,..,.,.'';';';';';'.,.,.,;l;';'1!!dasdasasshorttext'"
+# print(sanitizedString(test))
+
+
