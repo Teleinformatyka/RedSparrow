7caa3022f11ae20ea235f42c17676c8bf46b09c7
Yuriy Ganushevich <gan.yuriy@gmail.com>
2015-01-05 23:53:40 +0100

    class LogFormatter added

---
+++ redsparrow/log_formatter.py
@@ -0,0 +1,142 @@
+from __future__ import absolute_import, division, print_function, with_statement
+
+import logging
+import logging.handlers
+import sys
+import time
+
+from tornado.escape import _unicode
+from tornado.util import unicode_type, basestring_type
+
+try:
+    import curses
+except ImportError:
+    curses = None
+
+# Logger objects for internal tornado use
+access_log = logging.getLogger("tornado.access")
+app_log = logging.getLogger("tornado.application")
+gen_log = logging.getLogger("tornado.general")
+
+
+def _stderr_supports_color():
+    color = False
+    if curses and sys.stderr.isatty():
+        try:
+            curses.setupterm()
+            if curses.tigetnum("colors") > 0:
+                color = True
+        except Exception:
+            pass
+    return color
+
+
+class LogFormatter(logging.Formatter):
+    def __init__(self, color=True, *args, **kwargs):
+        logging.Formatter.__init__(self, *args, **kwargs)
+        self._color = color and _stderr_supports_color()
+        if self._color:
+            # The curses module has some str/bytes confusion in
+            # python3.  Until version 3.2.3, most methods return
+            # bytes, but only accept strings.  In addition, we want to
+            # output these strings with the logging module, which
+            # works with unicode strings.  The explicit calls to
+            # unicode() below are harmless in python2 but will do the
+            # right conversion in python 3.
+            fg_color = (curses.tigetstr("setaf") or
+                        curses.tigetstr("setf") or "")
+            if (3, 0) < sys.version_info < (3, 2, 3):
+                fg_color = unicode_type(fg_color, "ascii")
+            self._colors = {
+                logging.DEBUG: unicode_type(curses.tparm(fg_color, 4),  # Blue
+                                            "ascii"),
+                logging.INFO: unicode_type(curses.tparm(fg_color, 2),  # Green
+                                           "ascii"),
+                logging.WARNING: unicode_type(curses.tparm(fg_color, 3),  # Yellow
+                                              "ascii"),
+                logging.ERROR: unicode_type(curses.tparm(fg_color, 1),  # Red
+                                            "ascii"),
+            }
+            self._normal = unicode_type(curses.tigetstr("sgr0"), "ascii")
+
+    def format(self, record):
+        try:
+            record.message = record.getMessage()
+        except Exception as e:
+            record.message = "Bad message (%r): %r" % (e, record.__dict__)
+        assert isinstance(record.message, basestring_type)  # guaranteed by logging
+        record.asctime = time.strftime(
+            "%y%m%d %H:%M:%S", self.converter(record.created))
+        prefix = '[%(levelname)1.1s %(asctime)s %(module)s:%(lineno)d]' % \
+            record.__dict__
+        if self._color:
+            prefix = (self._colors.get(record.levelno, self._normal) +
+                      prefix + self._normal)
+
+        def safe_unicode(s):
+            try:
+                return _unicode(s)
+            except UnicodeDecodeError:
+                return repr(s)
+
+        formatted = prefix + " " + safe_unicode(record.message)
+        if record.exc_info:
+            if not record.exc_text:
+                record.exc_text = self.formatException(record.exc_info)
+        if record.exc_text:
+            # exc_text contains multiple lines.  We need to safe_unicode
+            # each line separately so that non-utf8 bytes don't cause
+            # all the newlines to turn into '\n'.
+            lines = [formatted.rstrip()]
+            lines.extend(safe_unicode(ln) for ln in record.exc_text.split('\n'))
+            formatted = '\n'.join(lines)
+        return formatted.replace("\n", "\n    ")
+
+
+def enable_pretty_logging(options=None, logger=None):
+    if options is None:
+        from tornado.options import options
+    if options.logging == 'none':
+        return
+    if logger is None:
+        logger = logging.getLogger()
+    logger.setLevel(getattr(logging, options.logging.upper()))
+    if options.log_file_prefix:
+        channel = logging.handlers.RotatingFileHandler(
+            filename=options.log_file_prefix,
+            maxBytes=options.log_file_max_size,
+            backupCount=options.log_file_num_backups)
+        channel.setFormatter(LogFormatter(color=False))
+        logger.addHandler(channel)
+
+    if (options.log_to_stderr or
+            (options.log_to_stderr is None and not logger.handlers)):
+        # Set up color if we are in a tty and curses is installed
+        channel = logging.StreamHandler()
+        channel.setFormatter(LogFormatter())
+        logger.addHandler(channel)
+
+
+def define_logging_options(options=None):
+    if options is None:
+        # late import to prevent cycle
+        from tornado.options import options
+    options.define("logging", default="info",
+                   help=("Set the Python log level. If 'none', tornado won't touch the "
+                         "logging configuration."),
+                   metavar="debug|info|warning|error|none")
+    options.define("log_to_stderr", type=bool, default=None,
+                   help=("Send log output to stderr (colorized if possible). "
+                         "By default use stderr if --log_file_prefix is not set and "
+                         "no other logging is configured."))
+    options.define("log_file_prefix", type=str, default=None, metavar="PATH",
+                   help=("Path prefix for log files. "
+                         "Note that if you are running multiple tornado processes, "
+                         "log_file_prefix must be different for each of them (e.g. "
+                         "include the port number)"))
+    options.define("log_file_max_size", type=int, default=100 * 1000 * 1000,
+                   help="max size of log files before rollover")
+    options.define("log_file_num_backups", type=int, default=10,
+                   help="number of log files to keep")
+
+    options.add_parse_callback(enable_pretty_logging)
